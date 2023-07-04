6d11a69e331e211180635ebc5b4cbff8920e7d7a
Angalexik <58977805+Angalexik@users.noreply.github.com>
2023-07-04 12:23:38 +0200

    `rustfmt` the benchmarks and add benchmarks for the transposition table

--- solver/benches/solver_benchmark.rs
+++ solver/benches/solver_benchmark.rs
@@ -1,62 +1,88 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use solver::Game;
+use solver::{FunnyTable, Game};
+
+pub fn bench_funnytable(c: &mut Criterion) {
+    c.bench_function("FunnyTable benchmark", |b| b.iter(|| FunnyTable::new()));
+}
 
 pub fn bench_3_3_3(c: &mut Criterion) {
     let mut game = Game::new_xo(black_box(3), black_box(3), black_box(3));
-    c.bench_function("3 3 3 benchmark", |b| b.iter(|| {
-        game.solver.clear_transpositions();
-        game.best_move()
-    }));
+    c.bench_function("3 3 3 benchmark", |b| {
+        b.iter(|| {
+            game.solver.clear_transpositions();
+            game.best_move()
+        })
+    });
 }
 
 pub fn bench_3_3_3_c4(c: &mut Criterion) {
     let mut game = Game::new_connect_four(black_box(3), black_box(3), black_box(3));
-    c.bench_function("3 3 3 Connect Four benchmark", |b| b.iter(|| {
+    c.bench_function("3 3 3 Connect Four benchmark", |b| {
         game.solver.clear_transpositions();
-        game.best_move()
-    }));
+        b.iter(|| game.best_move())
+    });
 }
 
 pub fn bench_4_4_3(c: &mut Criterion) {
     let mut game = Game::new_xo(black_box(4), black_box(4), black_box(3));
-    c.bench_function("4 4 3 benchmark", |b| b.iter(|| {
-        game.solver.clear_transpositions();
-        game.best_move()
-    }));
+    c.bench_function("4 4 3 benchmark", |b| {
+        b.iter(|| {
+            game.solver.clear_transpositions();
+            game.best_move()
+        })
+    });
 }
 
 pub fn bench_4_4_4(c: &mut Criterion) {
     let mut game = Game::new_xo(black_box(4), black_box(4), black_box(4));
-    c.bench_function("4 4 4 benchmark", |b| b.iter(|| {
-        game.solver.clear_transpositions();
-        game.best_move()
-    }));
+    c.bench_function("4 4 4 benchmark", |b| {
+        b.iter(|| {
+            game.solver.clear_transpositions();
+            game.best_move()
+        })
+    });
 }
 
 pub fn bench_4_4_4_c4(c: &mut Criterion) {
     let mut game = Game::new_connect_four(black_box(4), black_box(4), black_box(4));
-    c.bench_function("4 4 4 Connect Four benchmark", |b| b.iter(|| {
-        game.solver.clear_transpositions();
-        game.best_move()
-    }));
+    c.bench_function("4 4 4 Connect Four benchmark", |b| {
+        b.iter(|| {
+            game.solver.clear_transpositions();
+            game.best_move()
+        })
+    });
 }
 
 pub fn bench_5_5_3_o(c: &mut Criterion) {
     let mut game = Game::new_xo(black_box(5), black_box(5), black_box(3));
     game.place(black_box((2, 2)));
-    c.bench_function("5 5 3 benchmark as O", |b| b.iter(|| {
-        game.solver.clear_transpositions();
-        game.best_move()
-    }));
+    c.bench_function("5 5 3 benchmark as O", |b| {
+        b.iter(|| {
+            game.solver.clear_transpositions();
+            game.best_move()
+        })
+    });
 }
 
 pub fn bench_7_6_4_c4(c: &mut Criterion) {
     let mut game = Game::new_connect_four(7, 6, 4);
-    c.bench_function("7 6 4 Connect Four benchmark", |b| b.iter(|| {
-        game.solver.clear_transpositions();
-        game.best_move()
-    }));
+    c.bench_function("7 6 4 Connect Four benchmark", |b| {
+        b.iter(|| {
+            game.solver.clear_transpositions();
+            game.best_move()
+        })
+    });
 }
 
-criterion_group!(benches, bench_3_3_3, bench_3_3_3_c4, bench_4_4_3, bench_4_4_4, bench_4_4_4_c4, bench_5_5_3_o, bench_7_6_4_c4);
+criterion_group!(
+    benches,
+    bench_3_3_3,
+    bench_3_3_3_c4,
+    bench_4_4_3,
+    bench_4_4_4,
+    bench_4_4_4_c4,
+    bench_5_5_3_o,
+    bench_funnytable,
+    // bench_7_6_4_c4
+);
 criterion_main!(benches);
